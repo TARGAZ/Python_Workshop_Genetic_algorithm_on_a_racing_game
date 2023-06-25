ed555554f499fae3ad79dbf25561b0d203e5b942
Tanguy POINGT <poingt@et.esiea.fr>
2023-06-25 11:37:44 +0200

    Fishied project

--- Genetic_algorithm_on_a_racing_game/game.py
+++ Genetic_algorithm_on_a_racing_game/game.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 
+# Class representing the game map
 class Map:
     def __init__(self):
         self.ascii_map = [
@@ -31,12 +32,14 @@ class Map:
         self.window = pygame.display.set_mode((self.window_width, self.window_height))
         pygame.display.set_caption("Map Viewer")
 
+    # Display the map on the screen
     def display_map(self):
         for y, line in enumerate(self.ascii_map):
             for x, cell in enumerate(line):
                 pygame.draw.rect(self.window, self.colors[cell], (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
         pygame.display.flip()
 
+# Class representing a car
 class Car:
     def __init__(self):
         self.image = pygame.image.load("car.png")
@@ -45,64 +48,101 @@ class Car:
         self.height = self.image.get_height()
         self.x = 50
         self.y = 50
+        self.x_start = self.x
+        self.y_start = self.y
         self.direction = 0
-        self.speed = 0
-        self.max_speed = 2
+        self.speed = 1
+        self.max_speed = 5
+        self.min_speed = 1
         self.acceleration = 1
         self.deceleration = 0.5
-        self.reached_finish = False
         self.left_distance = 0
         self.right_distance = 0
         self.front_left_distance = 0
         self.front_right_distance = 0
         self.front_distance = 0
         self.radar_range = 500
+        self.nb_deaths = 0
+        self.nb_laps = 0
+        self.distance = 0
+        self.avg_speed = 0
 
+    # Draw the car on the game map
     def Draw(self, game_map):
         rotated_image = pygame.transform.rotate(self.image, math.degrees(self.direction))
         self.rect = rotated_image.get_rect()
         self.rect.center = (self.x + self.width / 2, self.y + self.height / 2)  # Modifier cette ligne
         game_map.window.blit(rotated_image, self.rect)
 
+    # Move the car based on its current speed and direction
     def move(self):
         self.x += self.speed * math.cos(self.direction)
         self.y -= self.speed * math.sin(self.direction)
 
+    # Accelerate the car
     def accelerate(self):
-        self.speed += self.acceleration
         if self.speed >= self.max_speed:
-            self.speed = self.max_speed
+            return
+        self.speed += self.acceleration
 
+    # Decelerate the car
     def decelerate(self):
+        if self.speed <= self.min_speed:
+            return
         self.speed -= self.deceleration
-        if self.speed <= 0:
-            self.speed = 0
 
+    # Steer the car to the left
     def steer_left(self):
         self.direction += math.radians(5)
 
+    # Steer the car to the right
     def steer_right(self):
         self.direction -= math.radians(5)
 
+    # Handle collision detection with the game map
     def Collision(self, game_map):
         car_rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
         for y, line in enumerate(game_map.ascii_map):
             for x, cell in enumerate(line):
-                if cell != "R" and cell != "S":
+                if cell != "R" and cell != "S" and cell != "F":
                     cell_rect = pygame.Rect(x * game_map.cell_size, y * game_map.cell_size, game_map.cell_size, game_map.cell_size)
                     if car_rect.colliderect(cell_rect):
                         # Collision detected, reset car position to start
-                        self.x = 50
-                        self.y = 50
+                        self.x = self.x_start
+                        self.y = self.y_start
                         self.direction = 0
-                        self.speed = 0
+                        self.speed = 1
+                        self.nb_deaths += 1
                 if cell == "F":
                     cell_rect = pygame.Rect(x * game_map.cell_size, y * game_map.cell_size, game_map.cell_size, game_map.cell_size)
                     if car_rect.colliderect(cell_rect):
-                        self.reached_finish = True
-                        print("Finish reached!")
+                        self.x = self.x_start
+                        self.y = self.y_start
+                        self.direction = 0
+                        self.speed = 1
+                        self.nb_laps += 1
 
+    # Calculate the average speed of the car
+    def calculate_avg_speed(self):
+        self.avg_speed += self.speed
+
+    # Calculate the distances traveled by the car
+    def calculate_distances(self):
+        self.distance += math.sqrt((self.x_start - self.x) ** 2 + (self.y_start - self.y) ** 2)
+
+    # Calculate the reward for the car based on its performance
+    def calculate_reward(self, game_map):
+        reward = 0
+        reward += self.nb_deaths * -1000
+        reward += self.nb_laps * 1000
+        reward += self.avg_speed * 1000
+        reward += self.distance
+        reward /= 10000
+
+        return reward
+
+    # Draw a radar beam on the game map
     def draw_radar_beam(self, game_map, distance, angle):
         start_x = self.x + self.width / 2
         start_y = self.y + self.height / 2
@@ -110,6 +150,7 @@ class Car:
         end_y = start_y - distance * math.sin(angle)
         pygame.draw.line(game_map.window, (255, 255, 255), (start_x, start_y), (end_x, end_y), 2)
 
+    # Perform radar scanning to detect obstacles
     def radar(self, game_map):
         chosen_angle = [0, 45, 90, 315, 270]
         for angle in chosen_angle:
--- Genetic_algorithm_on_a_racing_game/main.py
+++ Genetic_algorithm_on_a_racing_game/main.py
@@ -1,58 +1,102 @@
-import pygame.key
+# Importing required modules
+import pygame
+import os
+import neat
+import time
 
-from game import *
-from pygame.locals import *
+# Global variable to keep track of the generation
+generation = 0
 
-game_map = Map()
-car = Car()
+# Function to run the car simulation
+def run_car(genomes, config):
+    nets = []  # Neural networks
+    cars = []  # Car objects
 
-# Main loop
-running = True
-clock = pygame.time.Clock()
+    # Create neural networks and car objects for each genome
+    for id, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        g.fitness = 0
+        cars.append(Car())
 
-while running:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-        elif event.type == KEYDOWN:
-            if event.key == pygame.K_UP:
-                car.accelerate()
-            if event.key == pygame.K_DOWN:
-                car.decelerate()
-            if event.key == pygame.K_LEFT:
-                car.steer_left()
-            if event.key == pygame.K_RIGHT:
-                car.steer_right()
-            if event.key == pygame.K_ESCAPE:
-                running = False
-
-    car.move()
-
-    # Check collision
-    car.Collision(game_map)
-
-    # Efface l'écran
-    game_map.window.fill((255, 255, 255))
-
-    # Affiche la carte
+    game_map = Map()
+    pygame.init()
+    screen = pygame.display.set_mode((game_map.window_width, game_map.window_height))
+    clock = pygame.time.Clock()
+    generation_font = pygame.font.SysFont("Arial", 20)
     game_map.display_map()
 
-    # Affiche la voiture
-    car.Draw(game_map)
+    global generation
+    generation += 1
+    start_time = time.time()
+    skip = False
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+                if event.key == pygame.K_SPACE:
+                    skip = True
 
-    #radar
-    car.radar(game_map)
+        if skip:
+            break
 
-    print("left_distance: " + str(car.left_distance))
-    print("right_distance: " + str(car.right_distance))
-    print("front_left_distance: " + str(car.front_left_distance))
-    print("front_right_distance: " + str(car.front_right_distance))
-    print("front_distance: " + str(car.front_distance))
+        # Activate neural networks for each car and perform corresponding actions
+        for x, car in enumerate(cars):
+            output = nets[x].activate((car.left_distance, car.right_distance, car.front_left_distance, car.front_right_distance, car.front_distance))
+            i = output.index(max(output))
+            if i == 0:
+                car.accelerate()
+            elif i == 1:
+                car.decelerate()
+            elif i == 2:
+                car.steer_left()
+            elif i == 3:
+                car.steer_right()
 
+        game_map.window.fill((255, 255, 255))
 
-    # Rafraîchit l'affichage
-    pygame.display.flip()
-    clock.tick(60)  # Limite le taux de rafraîchissement à 60 FPS
+        game_map.display_map()
 
-pygame.quit()
+        # Update car positions, check collisions, calculate distances, and draw cars
+        for x, car in enumerate(cars):
+            if car.nb_deaths >= 1:
+                cars.pop(x)
+                nets.pop(x)
+                genomes.pop(x)
+                break
+
+            car.move()
+            car.Collision(game_map)
+            car.calculate_distances()
+            car.calculate_avg_speed()
+            genomes[x][1].fitness += car.calculate_reward(game_map)
+            car.radar(game_map)
+            car.Draw(game_map)
+
+        if len(cars) == 0:
+            break
+
+        # Display generation information on the screen
+        text = generation_font.render("Generation: " + str(generation), True, (255, 255, 255))
+        text_rect = text.get_rect()
+        text_rect.center = (game_map.window_width / 2, 20)
+        screen.blit(text, text_rect)
+
+        pygame.display.flip()
+        clock.tick(20)
+
+if __name__ == "__main__":
+    config_path = "config.txt"
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    # Run the car simulation using NEAT algorithm
+    p.run(run_car, 40000000000000000)
